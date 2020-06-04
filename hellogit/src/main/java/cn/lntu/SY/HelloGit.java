87205e699516d93ad4605d377e5af747a8cfd07b
Administrator <1192117398@qq.com>
2020-06-04 15:38:13 +0800

    添加文件

--- hellogit/src/main/java/cn/lntu/SY/HelloGit.java
+++ hellogit/src/main/java/cn/lntu/SY/HelloGit.java
@@ -5,5 +5,6 @@ import java.sql.SQLOutput;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("you ok");
+        System.out.println("oneDemo");
     }
 }
